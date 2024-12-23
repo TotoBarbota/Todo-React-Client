d1dca2e21cb72934da0a2d000005de0f24c34fd1
Thomas Karagiannis <thomaskaragiannis@hotmail.gr>
2024-12-23 20:55:32 +0200

    typo Number => number

--- Learning the React-Basics/todo-app/src/Components/counter/CounterButton.tsx
+++ Learning the React-Basics/todo-app/src/Components/counter/CounterButton.tsx
@@ -1,14 +1,12 @@
 import './CounterButton.css'
 type props = {
-    incrementValue: Number,
+    incrementValue: number,
     setfunction: Function
 }
 
-export default function Button(props: any) {
+export default function Button(props: props) {
     let buttnonclassname = "button-add"
-    if(props.incrementValue < 0){
-        buttnonclassname = "button-subract"
-    }
+    if(props.incrementValue < 0) buttnonclassname = "button-subract"
     return (
         <button className={buttnonclassname} onClick={() => props.setfunction(props.incrementValue)}>{props.incrementValue}</button>
     )}
